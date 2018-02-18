f1a6167d34e2300b95ad976eff88c1c84e1e8446
aiterasaki <minicoopers0716@hotmail.com>
2018-02-18 16:55:12 +0900

    コンフリクト解消分

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,8 @@ module Bcl
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-<<<<<<< HEAD
     config.i18n.default_locale = :ja
 
-=======
->>>>>>> 7e378a60a465631ea839b1636721eef303e60f5b
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
