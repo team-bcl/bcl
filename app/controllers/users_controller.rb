1cb0a81faf6e215ba83373dbc7674394bc9bd27b
aiterasaki <minicoopers0716@hotmail.com>
2018-02-24 15:08:48 +0900

    会員情報修正画面　作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+	def show
+		@user = current_user
+	end
+
+	# def edit
+	# 	@user = User.find(params[:id])
+	# end
+
+	# def user_params
+	# 	# params.require(:user).permit(
+	# 	# :first_name,
+	# 	# :last_name,
+	# 	# :first_name_kana,
+	# 	# :last_name_kana,
+	# 	# :email,
+	# 	# :phone_number,
+	# 	# :zip_code,
+	# 	# :prefectures,
+	# 	# :address_city,
+	# 	# :address_building
+	# 	# )
+	# end
 end
