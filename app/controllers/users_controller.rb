5d88cff878d39ea83a9d5a20bf907be4696d67cb
aiterasaki <minicoopers0716@hotmail.com>
2018-02-25 19:20:56 +0900

    退会処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
 		@user = current_user
 	end
 
+	def destroy
+    @user = User.find(params[:id])
+
+    if @user.destroy
+        redirect_to root_url, notice: "User deleted."
+    end
+  end
+
 	# def edit
 	# 	@user = User.find(params[:id])
 	# end
