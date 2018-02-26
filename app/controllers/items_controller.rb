64c89903283784a26ef543fdb904613032f0e5ef
endo123 <endo@gmail.com>
2018-02-26 05:31:03 +0000

    design,item(view2)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,62 @@
 class ItemsController < ApplicationController
+		before_action :authenticate_user!, only:[:new,:create]
+		#before_action :admin_only, only:[:new,:create]
+		before_action :set_item, only: [:show, :update, :edit, :delete]
+	def new
+		@item = Item.new
+		10.times{
+		@item.track_list.build
+		}
+	end
+	def create
+		@item = Item.new(item_params)
+		@item.user_id = current_user.id
+		if @item.save
+			redirect_to root_path
+		else
+			redirect_to new_item_path
+		end
+	end
 	def index
-		@items =Item.all.order(created_at: 'desc')
+		@items =Item.all.order(created_at: 'desc').page(params[:page]).per(12)
+					.genres_search(params[:genre].presence)
+					.item_name_search(params[:search_word].presence)
 	end
 
 	def show
-		@item = Item.find(params[:id])
+		@cart_item = CartItem.new
 		@tracklists = @item.track_lists
 	end
 
+	def edit
+	end
+	
+	def update
+		@item.update(item_params)
+		redirect_to root_path
+	end
+
+	def destroy
+		@item.destroy
+		redirect_to root_path
+	end
+
+	private
+	def set_item
+		@item = Item.find(params[:id])
+	end
 
 	def item_params
-	params.require(:item).permit(
-		:item_image
-		)
+		params.require(:item).permit(
+		:item_name,:item_name_kana,:price,
+		:artist_name,:artist_name_kana,:genres,:release_date,
+		:label,:user_id,:stock,:is_available,:item_image, track_lists_attributes: [:id,:_destroy,:track_name])
+	end 
+
+	def admin_only
+		if current_user.admin_flag == true
+			redirect_to root_path
+		end
 	end
 end
 
