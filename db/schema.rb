a13e7e7f9a9c14f75016a7313c33207f6ff13952
aiterasaki <minicoopers0716@hotmail.com>
2018-02-26 15:22:44 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20180221043504) do
+
+ActiveRecord::Schema.define(version: 20180225062016) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
