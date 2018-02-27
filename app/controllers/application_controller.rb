8492b92d14fa0163ab79b77b9c50f9f8210a57d9
aiterasaki <minicoopers0716@hotmail.com>
2018-02-27 11:33:53 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,22 @@ class ApplicationController < ActionController::Base
 		devise_parameter_sanitizer.permit :account_update, keys: added_attrs
 
     end
+
+	# def after_sign_in_path_for(resource)
+	# 	if current_user.admin_flag == true
+	# 		items_path
+	# 	else
+	# 		new_admin_user_session_path # ログイン後に遷移するpathを設定
+	# 	end 
+	# end
+
+	# def after_sign_out_path_for(resource)
+	# 	items_path # ログアウト後に遷移するpathを設定
+	# end
 end
+
+
+# http://localhost:3000/admin/ へアクセス
+
+# email: admin@example.com
+# password: password
