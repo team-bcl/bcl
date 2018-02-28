4c2781d67a178d41375fdf86cf9e23df93f79814
yasui321 <sample@sample.com>
2018-02-28 07:02:14 +0000

    オーダーと購入履歴のビュー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,8 @@ class UsersController < ApplicationController
 
 	def show
 		@user = current_user
+		@order_history = CartItem.order_history @user
+		# binding.pry
 	end
 
 	def destroy
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,7 @@ class Order < ApplicationRecord
 	has_many :cart_item
 	belongs_to :user
 	enum status:{ deliver: 1, shipment: 2}
+
+	include JpPrefecture
+	jp_prefecture :prefectures
 end
